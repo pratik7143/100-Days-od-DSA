33f5ede9fd1cf576b9f5de6d177d40b42f3e114d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-08 21:36:03 +0530

    Day 7 problem 1

---
+++ Day7problem1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int fib(int n)
+{
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+    int n = 10;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fib(i));
+    }
+    return 0;
+}
