362c5b977a6a0f39ae4fbe93ce602c49ba1f1e6e
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-09 23:11:51 +0530

    Day 9 problem 2

--- Day9problem2.c
+++ Day9problem2.c
@@ -2,7 +2,8 @@ void reverseString(char* s, int sSize)
 {
     int left = 0;
     int right = sSize - 1;
-    while (left < right) {
+    while (left < right) 
+    {
         char temp = s[left];
         s[left] = s[right];
         s[right] = temp;
