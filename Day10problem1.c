4f460f49e24eb43b68455903ba2d7d4d94f97752
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-10 21:41:25 +0530

    Day 10 problem 1

---
+++ Day10problem1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int i, j, flag = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    i = 0;
+    j = strlen(str) - 1;
+
+    while (i < j) {
+        if (str[i] != str[j]) {
+            flag = 0;
+            break;
+        }
+        i++;
+        j--;
+    }
+
+    if (flag == 1)
+        printf("Palindrome\n");
+    else
+        printf("Not Palindrome\n");
+
+    return 0;
+}
