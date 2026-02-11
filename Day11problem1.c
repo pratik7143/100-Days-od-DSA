8fcae4553b4d1867e25fea20862cad1449a9cb58
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-11 22:42:02 +0530

    Add files via upload

---
+++ Day11problem1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int r, c;
+
+    // Input number of rows and columns
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    // Input first matrix
+    printf("Enter elements of Matrix A:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of Matrix B:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Calculate sum
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Output the result
+    printf("Resultant Matrix (A + B):\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
