7149e1838744a9f331a26d0e2be2a3b05566e46e
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-13 23:17:48 +0530

    Day 13 problem 2

--- Day13problem2.c
+++ Day13problem2.c
@@ -1,6 +1,4 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+
 int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
     
     int m = matrixSize;
