f2247d222034f64cb983be176037fe931ede9e2c
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-16 21:33:02 +0530

    Create Day16problem2.c

---
+++ Day16problem2.c
@@ -0,0 +1,22 @@
+void reverse(int* nums, int start, int end) {
+    while(start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+    if(numsSize <= 1) return;
+
+    k = k % numsSize; 
+    reverse(nums, 0, numsSize - 1);
+    
+    // Step 2: Reverse first k elements
+    reverse(nums, 0, k - 1);
+    
+    // Step 3: Reverse remaining elements
+    reverse(nums, k, numsSize - 1);
+}
