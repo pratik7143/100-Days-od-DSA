ba57e4e391d9ea3fbd838bb08268b1861be40edb
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-16 21:34:18 +0530

    Day 16 problem 2

--- Day16problem2.c
+++ Day16problem2.c
@@ -13,7 +13,6 @@ void rotate(int* nums, int numsSize, int k) {
     reverse(nums, 0, numsSize - 1);
     // Step 2: Reverse first k elements
     reverse(nums, 0, k - 1);
-    
     // Step 3: Reverse remaining elements
     reverse(nums, k, numsSize - 1);
 }
