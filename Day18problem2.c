635e0aeb941a0da1e8d4712eab01412ed3380d06
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-18 22:09:05 +0530

    Update Day18problem2.c

--- Day18problem2.c
+++ Day18problem2.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
-    
     *returnSize = numsSize;
     
     int* result = (int*)malloc(numsSize * sizeof(int));
