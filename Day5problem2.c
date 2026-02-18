87e3788bb97d9812084c2395ebfce9cd3f36dde5
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-18 22:15:11 +0530

    Update Day5problem2.c

--- Day5problem2.c
+++ Day5problem2.c
@@ -3,7 +3,8 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     int j = n - 1;          
     int k = m + n - 1;      
     while (j >= 0) {
-        if (i >= 0 && nums1[i] > nums2[j]) {
+        if (i >= 0 && nums1[i] > nums2[j]) 
+        {
             nums1[k] = nums1[i];
             i--;
         } else {
