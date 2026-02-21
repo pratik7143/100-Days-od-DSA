18ad3bdaf17b63666f7af6337e93b8a8b5325465
Pratik Agarwal <pratikagarwal0603@gmail.com>
2026-02-21 18:43:33 +0530

    '

--- Day21problem2.c
+++ Day21problem2.c
@@ -3,7 +3,7 @@ struct ListNode* middleNode(struct ListNode* head) {
     struct ListNode *fast = head;
     while (fast != NULL && fast->next != NULL) {
         slow = slow->next;          // move 1 step
-        fast = fast->next->next;    // move 2 steps
+        fast = fast->next->next;    
     }
 
     return slow; // slow will be at middle
